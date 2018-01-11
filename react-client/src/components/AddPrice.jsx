558c64cc8eeccaeecf082739993dae97e5b17163
Ergo117 <peter.irie.wang@gmail.com>
2018-01-10 20:10:37 -0800

    addPrice code cleaning

--- react-client/src/components/AddPrice.jsx
+++ react-client/src/components/AddPrice.jsx
@@ -4,45 +4,31 @@ class AddPrice extends React.Component {
   constructor (props) {
     super (props);
        this.state = {
-        price: '$'
+        price: ''
        };
     }
 
-  budget (e) {
+  priceHandler (e) {
     this.setState({
-      price: '$'
-    })
-    console.log('price range', this.state.price);
-  }
-
-  someBills (e) {
-    this.setState({
-      price: '$$'
-    })
-    console.log('price range', this.state.price);
-  }
-
-  playMoney (e) {
-    this.setState({
-      price: '$$$'
-    })
-    console.log('price range', this.state.price);
-  }
-
-  ballin (e) {
-    this.setState({
-      price: '$$$$'
-    })
-    console.log('price range', this.state.price);
+      price: e.target.value
+    });
   }
 
   render () {
     return (
       <div>
-        <button onClick={this.budget.bind(this)}>Budget</button>
-        <button onClick={this.someBills.bind(this)}>Got Bills</button>
-        <button onClick={this.playMoney.bind(this)}>Play Money</button>
-        <button onClick={this.ballin.bind(this)}>I'm Ballin</button>
+        <label>
+          <input type="radio" name="price" value="$" onClick={() => {this.priceHandler.bind(this)}}/>$
+        </label>
+        <label>
+          <input type="radio" name="price" value="$$" onClick={() => {this.priceHandler.bind(this)}}/>$$
+        </label>
+        <label>
+          <input type="radio" name="price" value="$$$" onClick={() => {this.priceHandler.bind(this)}}/>$$$
+        </label>
+        <label>
+          <input type="radio" name="price" value="$$$$" onClick={() => {this.priceHandler.bind(this)}}/>$$$$
+        </label>
       </div>
 
     )

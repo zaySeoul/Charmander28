f9469195f5f138d953fc88c7bc5e0ad43b98c43d
Zay <zayseoul@gmail.com>
2018-01-12 19:35:05 -0500

    Added passport into package.json Edited webpack to original

--- webpack.config.js
+++ webpack.config.js
@@ -15,19 +15,47 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?/,
+        // include: SRC_DIR,
+        // loader: 'babel-loader',
+        // query: {
+        //   presets: ['react', 'es2015'],
+        // },
         loaders: ['babel-loader?presets[]=react,presets[]=es2015'],
-        query: {
-          presets: ['es2015', 'react', 'stage-0'],
-        },
-      },
-      {
-        test: /\.png$/,
-        loader: 'url-loader?limit=100000',
-      },
-      {
-        test: /\.jpg$/,
-        loader: 'file-loader',
       },
     ],
   },
 };
+
+// const webpack = require('webpack');
+//
+// const path = require('path');
+//
+// const SRC_DIR = path.join(__dirname, '/react-client/src');
+// const DIST_DIR = path.join(__dirname, '/react-client/dist');
+//
+// module.exports = {
+//   entry: `${SRC_DIR}/index.jsx`,
+//   output: {
+//     filename: 'bundle.js',
+//     path: DIST_DIR,
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?/,
+//         loaders: ['babel-loader?presets[]=react,presets[]=es2015'],
+//         query: {
+//           presets: ['es2015', 'react', 'stage-0'],
+//         },
+//       },
+//       {
+//         test: /\.png$/,
+//         loader: 'url-loader?limit=100000',
+//       },
+//       {
+//         test: /\.jpg$/,
+//         loader: 'file-loader',
+//       },
+//     ],
+//   },
+// };

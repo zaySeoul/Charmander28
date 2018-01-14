4df75d3e7cd0b82cdde27a25b8b45374f728423b
Tejaswi <tejaswi.reddy6@gmail.com>
2018-01-14 11:09:22 -0800

    resoled merge conflicts

--- react-client/src/components/EatView.jsx
+++ react-client/src/components/EatView.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-//import Gallery from 'react-grid-gallery';
 import EatViewListEntry from './EatViewListEntry.jsx';
 
 let EatView = (props) => {
 
   return (
-    <div>
-      {props.eat.map(item =>
-        <EatViewListEntry key={props.eat.indexOf(item)} item={item} />
-      )}
+    <div className="container">
+      <div className="row">
+        {props.eat.map(item =>
+          <EatViewListEntry key={props.eat.indexOf(item)} item={item} />
+        ) }
+      </div>
     </div>
   );
 };

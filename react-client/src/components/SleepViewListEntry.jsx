39823f6bf70b89edabcbf5d65de0b487dc77b08e
Tejaswi <tejaswi.reddy6@gmail.com>
2018-01-14 12:28:53 -0800

    updated the name for url

--- react-client/src/components/SleepViewListEntry.jsx
+++ react-client/src/components/SleepViewListEntry.jsx
@@ -5,7 +5,7 @@ let SleepViewListEntry = (props) => {
     <div className="col-sm-4">
       <h3 className="title">{ props.item.name }</h3>
       <img src={props.item.imageUrl.replace('/o.jpg', '/m.jpg')} alt="" /> <br/>
-      <a href={props.item.url }> Hello </a>
+      <a href={props.item.url }> {props.item.name} </a>
     </div>
   );
 };
